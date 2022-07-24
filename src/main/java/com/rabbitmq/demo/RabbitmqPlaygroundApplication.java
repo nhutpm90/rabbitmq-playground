dc0f83e1dc0e3c839eba5ae589dd7ba2721c19ae
nhut_pham <nhut.pham@ilogic.vn>
2022-07-24 14:49:37 +0700

    init proj

---
+++ src/main/java/com/rabbitmq/demo/RabbitmqPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPlaygroundApplication.class, args);
+	}
+
+}
